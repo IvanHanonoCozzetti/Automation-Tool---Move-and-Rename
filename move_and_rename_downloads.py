3fb2bb238c38952675f46e7532b4bb7d40fd3741
IvanHanonoCozzetti <64793737+IvanHanonoCozzetti@users.noreply.github.com>
2020-11-27 16:14:59 +0200

    Add files via upload

---
+++ move_and_rename_downloads.py
@@ -0,0 +1,35 @@
+from watchdog.observer import observer
+from watchdog.events import FileSystemEventHandler
+
+import os
+import json
+import time
+
+class MyHandler(FileSystemEventHandler):
+	i = 1
+	def on_modified(self, event):
+		new_name = "new_file_" + str(self.i) + ".txt"
+		for filename in os.listdir(folder_to_track):
+			file_exists = os.path.isfile(folder_destination + "/" + new_name)
+			while file_exists:
+				self.i += 1
+				new_name = "new_file_" + str(self.i) + ".txt"
+				file_exists = os.path.isfile(folder_destination + "/" + new_name)
+
+				src = folder_to_track + "/" + filename
+				new_destination = folder_destination + "/" + filename
+				os.rename(src, new_destination)
+
+folder_to_track = "/users/Ivan/Desktop/myFolder"
+folder_destination = "/users/Ivan/Desktop/myFolder"
+event_handler = MyHandler()
+observer = Observer()
+observer.schedule(even_handler, folder_to_track, recursive=True)
+observer.start
+
+try:
+	while True:
+		time.sleep(10)
+	except KeyboardInterrupt:
+		observer.stop()
+	observer.join()
